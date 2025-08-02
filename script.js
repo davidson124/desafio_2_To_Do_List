f520acada1212c4430db41e51496128b6e5e3f36
davidson124 <daferdinat@gmail.com>
2025-08-02 11:51:32 -0500

    Se ha llevado a cabo un to to list, en el cual se inplementaron estilos css y llamados al html con javascript, queda pendiente realizar una condicion para cuando no se ingresa texto

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ let inputCaja = document.querySelector('#inputCaja');
 let lista = document.querySelector('#lista');
 
 inputCaja.addEventListener("keyup", function(event){
-if (event.key=="enter"){
+if (event.key == "Enter") {
     addItem(this.value)
-    this.value=""
+    this.value = ""
     }
 })
-let addItem = (inputCaja)=> {
-    let listaItem = document.createElement("li");
-    listaItem.innerHTML = `${inputCaja}<i><i>`;
-    lista.appendChild(listaItem);
+let addItem = (inputCaja) => {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `${inputCaja}<i></i>`;
+
+    listItem.addEventListener("click", function(){
+        this.classList.toggle('dode');
+    })
+
+    listItem.querySelector("i").addEventListener("click", function(){
+        listItem.remove();
+    })
+
+    lista.appendChild(listItem);
 }
